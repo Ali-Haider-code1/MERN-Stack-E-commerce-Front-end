30c8ff3be006dba9d34c589d55fb0574745093a0
Ali-Haider-code1 <alihaiderawan1345@gmail.com>
2024-04-26 16:50:51 +0500

    Complete Product list added along with navbar,pagination and filters 26/4

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Navbar from "../features/Navbar/navbar";
+import ProductList from "../features/product-list/ProductList";
+
+const Home = () => {
+    return (
+        <Navbar>
+            <ProductList></ProductList>
+        </Navbar>
+    )
+}
+
+export default Home; 
