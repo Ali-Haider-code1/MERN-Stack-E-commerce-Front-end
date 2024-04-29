4283a29a2dbffa139836e0128482e501989750da
Ali-Haider-code1 <alihaiderawan1345@gmail.com>
2024-04-29 18:56:27 +0500

    The website's routing add, as well as the login and register pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Login from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+    <RouterProvider router={router} />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import Navbar from "../features/Navbar/navbar";
 import ProductList from "../features/product-list/ProductList";
-
+import LoginPage from './LoginPage'
 const Home = () => {
     return (
         <Navbar>
-            <ProductList></ProductList>
+            <ProductList></ProductList>            
         </Navbar>
     )
 }
